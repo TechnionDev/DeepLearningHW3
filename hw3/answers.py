902c6d5919ed2731f4cc0398371d3eaad27b9344
Sari Hleihil <sarihl@lambda.csf.technion.ac.il>
2021-06-22 00:04:20 +0300

    most advacned

--- hw3/answers.py
+++ hw3/answers.py
@@ -112,12 +112,12 @@ def part2_vae_hyperparams():
     )
     # TODO: Tweak the hyperparameters to generate a former president.
     # ====== YOUR CODE: ======
-    hypers['batch_size'] = 100
-    hypers['h_dim'] = 128
+    hypers['batch_size'] = 64
+    hypers['h_dim'] = 64
     hypers['z_dim'] = 32
-    hypers['x_sigma2'] = 0.0008
-    hypers['learn_rate'] = 1e-3
-    hypers['betas'] = (0.9, 0.999)
+    hypers['learn_rate'] = 0.0002
+    hypers['betas'] = 0.9, 0.999
+    hypers['x_sigma2'] = 0.0002
     # ========================
     return hypers
 
